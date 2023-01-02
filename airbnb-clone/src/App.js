08027e24eedd93bfed96fc07a3b87b4337d7a62f
ipekozbay <20060401@stu.omu.edu.tr>
2023-01-02 18:41:22 +0300

    new cards added

--- airbnb-clone/src/App.js
+++ airbnb-clone/src/App.js
@@ -3,19 +3,33 @@ import Navbar from './components/Navbar';
 import React from 'react';
 import Hero from './components/Hero';
 import Card from './components/Card';
+import data from '../src/config/data'
+console.log(data)
 
 export default function App() {
+
+  const cards = data.map(item => {
+    return (
+      <Card
+        img={item.coverImg}
+        rating={item.stats.rating}
+        reviewCount={item.stats.reviewCount}
+        country={item.location}
+        title={item.title}
+        price={item.price}
+      />
+    )
+
+  })
+
   return (
     <div>
-      <Navbar/>
-      <Hero/>
-      <Card
-        rating = "5.0"
-        reviewCount ={6}
-        country = "USA"
-        title = "Life lessons with katie zaferes"
-        price = {136}
-      />
+      <Navbar />
+      <Hero />
+      <section className='cards-list'>
+        {cards}
+      </section>
+
     </div>
   );
 }
--- airbnb-clone/src/components/Card.js
+++ airbnb-clone/src/components/Card.js
@@ -4,12 +4,12 @@ export default function Card(props){
     
     return(
         <div className="card">
-            <img src="../images/katie-zaferes.png" className="card--image"/>
+            <img src={`../images/${props.img}`}className="card--image"/>
             <div className="card--stats">
-                <img src="../images/star.png" className="card--star"/> 
+                <img src="../images/star 1.png" className="card--star"/> 
                 <span className="gray">{props.rating}</span>
-                <span className="gray">{props.reviewCount}</span>
-                <span> . {props.country}</span>
+                <span className="gray">{props.reviewCount} •</span>
+                <span>{props.country}</span>
             </div>
             <p>{props.title}</p>
             <p><span className="bold">From {props.price}</span></p>
