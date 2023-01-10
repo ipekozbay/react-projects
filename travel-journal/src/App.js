f2113c20226d9ab63da074dff535ab97cc58d909
ipekozbay <20060401@stu.omu.edu.tr>
2023-01-10 14:52:04 +0300

    cart elements have been edited

--- travel-journal/src/App.js
+++ travel-journal/src/App.js
@@ -1,12 +1,26 @@
 import './App.css';
+import React from 'react';
 import Card from "./components/Card"
 import Navbar from "./components/Navbar"
+import data from '../src/config/data'
 
 export default function App() {
+
+const cards = data.map(item => {
+  return (
+    <Card
+      key = {item.id}
+      {...item}
+    />
+  )
+})
+
   return (
     <div className="App">
         <Navbar/>
-        <Card/>
+        <section className='cards-list'>
+        {cards}
+      </section>
     </div>
   );
 }
--- travel-journal/src/components/Card.js
+++ travel-journal/src/components/Card.js
@@ -1,9 +1,26 @@
 import React from "react"
 
 
-export default function Card(){
+export default function Card(props) {
 
-    return(
-        <h>i m the card comp</h>
+    return (
+        <div className="card">
+
+            <div className="card--image">
+                <img src={`../images/${props.imageUrl}`} className="card--image" />
+            </div>
+
+            <div className="card--content">
+                <div className="location">
+                    <p className="location-country">{props.location}</p>
+                    <a href={props.googleMapsUrl} className="maps-url">view on google maps</a>
+                </div>
+
+                <p className="title"> {props.title} </p>
+                <p className="date">{props.startDate} {props.endDate}</p>
+                <p className="description">{props.description}</p>
+            </div>
+
+        </div>
     )
 }
